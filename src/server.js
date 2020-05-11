8b2d2c50c8a34cc415e8087daba63d54cfc04d87
Thiago Roberto <thiago.nemo4code@gmail.com>
2020-05-11 20:35:52 -0300

    Adicionado rotas dos controllers e realizado ajuste no endpoint base do serviço http.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from "express";
-
 import routes from "./routes";
 
 const app = express();
 
-app.use("/api", routes);
+app.use(express.json());
+app.use(routes);
 
 export default app.listen(process.env.PORT || 3000);
