a89b8bbeba21e603a9b72cb6badc81d9abfc288a
Thiago Roberto <thiago.nemo4code@gmail.;com>
2020-05-14 17:39:45 -0300

    Revert "Adicionado rotas dos controllers e realizado ajuste no endpoint base do serviço http."
    This reverts commit 8b2d2c50c8a34cc415e8087daba63d54cfc04d87.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from "express";
+
 import routes from "./routes";
 
 const app = express();
 
-app.use(express.json());
-app.use(routes);
+app.use("/api", routes);
 
 export default app.listen(process.env.PORT || 3000);
